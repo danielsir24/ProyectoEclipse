6a6a5bafe269527bb052dc1f67cb3b279a6fe171
adrianrodrigueznav <adrianrdgz.nav@gmail.com>
2026-03-05 09:43:17 +0100

    Metodos Objeto

--- ProyectoPokemon/src/pokemon/Objeto.java
+++ ProyectoPokemon/src/pokemon/Objeto.java
@@ -2,23 +2,33 @@ package pokemon;
 
 public class Objeto {
 
-    private String nombre;
+	private String nombre;
+	private int bonusAtaque;
+	private int penalizacionDefensa;
 
-    private int bonusAtaque;
-    private int penalizacionDefensa;
+	public Objeto(String nombre, int bonusAtaque, int penalizacionDefensa) {
+		this.nombre = nombre;
+		this.bonusAtaque = bonusAtaque;
+		this.penalizacionDefensa = penalizacionDefensa;
+	}
 
-    public Objeto(String nombre, int bonusAtaque, int penalizacionDefensa) {
-        this.nombre = nombre;
-        this.bonusAtaque = bonusAtaque;
-        this.penalizacionDefensa = penalizacionDefensa;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public int getBonusAtaque() {
-        return bonusAtaque;
-    }
+	public int getBonusAtaque() {
+		return bonusAtaque;
+	}
 
-    public int getPenalizacionDefensa() {
-        return penalizacionDefensa;
-    }
+	public int getPenalizacionDefensa() {
+		return penalizacionDefensa;
+	}
+
+	public String obtenerInfo() {
+		return nombre + " (Ataque: +" + bonusAtaque + " | Defensa: -" + penalizacionDefensa + ")";
+	}
+
+	public boolean esOfensivo() {
+		return bonusAtaque > penalizacionDefensa;
+	}
 }
-
