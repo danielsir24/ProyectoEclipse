9558ea789c1c2bb3d3649e34a56ce010058b74d7
sirbumihutadaniel@gmail.com <sirbumihutadaniel@gmail.com>
2026-03-12 12:25:55 +0100

    Primera Escena cargada y el main y el main controller implemetnados

--- ProyectoPokemon/src/pokemon/Main.java
+++ ProyectoPokemon/src/pokemon/Main.java
@@ -1,10 +1,26 @@
 package pokemon;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends Application {
 
-	}
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Cargar el archivo FXML
+        Parent root = FXMLLoader.load(getClass().getResource("/EscenaLogin.fxml"));
 
+        Scene scene = new Scene(root);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
