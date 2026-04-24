7ea810c99209e210b28e93758e928a600adb2353
Juan Carlos Benítez Ortega <jcbenitezortega@gmail.com>
2026-04-24 12:57:03 +0200

    combate
    corrección de la clase combate

--- ProyectoPokemon/src/pokemon/Movimiento.java
+++ ProyectoPokemon/src/pokemon/Movimiento.java
@@ -1,31 +1,50 @@
 package pokemon;
 
 public class Movimiento {
+    private String nombre;
+    private int potencia;
+    private Tipo tipo; // CAMBIADO: Antes era int, ahora es Tipo
+    private String categoria; // "ATAQUE", "ESTADO", "MEJORA"
+    private int turnos; 
+    private int idMovimiento;
 
-	private String nombre;
-	private int potencia;
-	private int costeEstamina;
-	private int idMovimiento;
+    // Actualiza el constructor
+    public Movimiento(String nombre, int potencia, Tipo tipo, String categoria, int turnos, int idMovimiento) {
+        this.nombre = nombre;
+        this.potencia = potencia;
+        this.tipo = tipo;
+        this.categoria = categoria;
+        this.turnos = turnos;
+        this.idMovimiento = idMovimiento;
+    }
 
-	public Movimiento(String nombre, int potencia, int costeEstamina, int idMovimiento) {
-		this.nombre = nombre;
-		this.potencia = potencia;
-		this.costeEstamina = costeEstamina;
-		this.idMovimiento = idMovimiento;
-	}
+    // REQUISITO: El coste de estamina es dinámico
+    public int getCosteEstamina() {
+        if ("ATAQUE".equalsIgnoreCase(categoria)) {
+            return potencia / 2;
+        } else {
+            return turnos * 10;
+        }
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
-
-	public int getPotencia() {
-		return potencia;
-	}
-
-	public int getCosteEstamina() {
-		return costeEstamina;
-	}
-	public int getIdMovimiento() {
-		return idMovimiento;
-	}
+    //Getters
+    public String getNombre() {
+    	return nombre; 
+    }
+    
+    public int getPotencia() { 
+    	return potencia;
+    }
+    
+    public Tipo getTipo() { 
+    	return tipo; 
+    }
+    
+    public String getCategoria() { 
+    	return categoria; 
+    }
+    
+    public int getIdMovimiento() { 
+    	return idMovimiento; 
+    }
 }
