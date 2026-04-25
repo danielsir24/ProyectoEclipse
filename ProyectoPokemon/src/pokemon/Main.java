951914dab293223cd06a2497c81ec0a08c65fe61
danielsir24 <sirbumihutadaniel@gmail.com>
2026-04-25 13:01:08 +0200

    EscenaCombate y CombateController modificados
    He añadido el boton de huir con sus resspectivo comentarios, tambien he modificado una cosa en la escenaCombate que es añadir todos los fx y todo slos action para que funcione, por ahora solo funciona el boton de huir, he tenido que cambiart una cosa tambien el controller combate que es un hbox por un vbox

--- ProyectoPokemon/src/pokemon/Main.java
+++ ProyectoPokemon/src/pokemon/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
+ 
 		// Cargar escena de Login
 		Parent root = FXMLLoader.load(getClass().getResource("/EscenaLogin.fxml"));
 		primaryStage.setTitle("Login Pokémon");
